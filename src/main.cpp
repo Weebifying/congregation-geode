144a26fbfce1eafa38d806f28afab2f636752d2e
Weebifying <weebifylmao123@gmail.com>
2024-03-08 19:32:21 +0700

    bro

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ bool isMainLevel = false;
 int mainLevels[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,1001,1002,1003,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,3001,4001,4002,4003,5001,5002,5003,5004};
 
 class $modify(PlayLayer) {
-    static CCScene* scene(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+    bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
 		auto chance = Mod::get()->getSettingValue<double>("chance");
 		if (rand()/(RAND_MAX+1.0) < chance/100) {
 			orgLevel = level;
@@ -24,7 +24,7 @@ class $modify(PlayLayer) {
 			else isMainLevel = false;
 		}
 
-        return PlayLayer::scene(level, useReplay, dontCreateObjects);
+        return PlayLayer::init(level, useReplay, dontCreateObjects);
     }
 };
 
